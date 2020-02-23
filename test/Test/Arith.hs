0235728f0d4b50989a73acb43b95d321fd0f6a46
AlyaNovikova <aleksnovikova@gmail.com>
2020-02-23 15:05:55 +0300

    HW01

--- test/Test/Arith.hs
+++ test/Test/Arith.hs
@@ -1,11 +1,11 @@
-module Test.Arith where 
+module Test.Arith where
 
 import Test.HUnit (Assertion, (@?=))
 import Arith (evaluate, parseNum, parseOp, parseSum, parseMult, Operator (..), AST (..), toPostfix, fromPostfix)
 
-unit_evaluate :: Assertion 
-unit_evaluate = do 
-    evaluate "1" @?= Just 1 
+unit_evaluate :: Assertion
+unit_evaluate = do
+    evaluate "1" @?= Just 1
     evaluate "1+2" @?= Just (1+2)
     evaluate "2+4+8" @?= Just (2+4+8)
     evaluate "11+22" @?= Just (11+22)
@@ -20,30 +20,30 @@ unit_evaluate = do
     evaluate "12+(23*(34)+456)" @?= Just (12+(23*(34)+456))
     evaluate "((1-(2*3))+4)" @?= Just ((1-(2*3))+4)
 
-unit_parseNum :: Assertion 
-unit_parseNum = do 
+unit_parseNum :: Assertion
+unit_parseNum = do
     parseNum "7" @?= Just (Num 7, "")
     parseNum "12+3" @?= Just (Num 12, "+3")
     parseNum "007" @?= Just (Num 7, "")
-    parseNum "+3" @?= Nothing 
-    parseNum "a" @?= Nothing 
+    parseNum "+3" @?= Nothing
+    parseNum "a" @?= Nothing
 
-unit_parseOp :: Assertion 
-unit_parseOp = do 
+unit_parseOp :: Assertion
+unit_parseOp = do
     parseOp "+1" @?= Just (Plus, "1")
     parseOp "**" @?= Just (Mult, "*")
     parseOp "-2" @?= Just (Minus, "2")
     parseOp "/1" @?= Just (Div, "1")
-    parseOp "12" @?= Nothing 
+    parseOp "12" @?= Nothing
 
-unit_parseMult :: Assertion 
-unit_parseMult = do 
+unit_parseMult :: Assertion
+unit_parseMult = do
     parseMult "1*2*3" @?= Just (BinOp Mult (Num 1) (BinOp Mult (Num 2) (Num 3)), "")
     parseMult "123" @?= Just (Num 123, "")
     parseMult "1*2+3*4" @?= Just (BinOp Mult (Num 1) (Num 2), "+3*4")
 
-unit_parseSum :: Assertion 
-unit_parseSum = do 
+unit_parseSum :: Assertion
+unit_parseSum = do
     parseSum "1*2*3"   @?= Just (BinOp Mult (Num 1) (BinOp Mult (Num 2) (Num 3)), "")
     parseSum "123"     @?= Just (Num 123, "")
     parseSum "1*2+3*4" @?= Just (BinOp Plus (BinOp Mult (Num 1) (Num 2)) (BinOp Mult (Num 3) (Num 4)), "")
@@ -56,11 +56,12 @@ unit_toPostfix = do
     toPostfix (BinOp Plus (BinOp Mult (Num 1) (Num 2)) (Num 3)) @?= "1 2 * 3 +"
     toPostfix (BinOp Plus (BinOp Mult (BinOp Minus (Num 1) (Num 2)) (BinOp Div (Num 3) (Num 4))) (BinOp Plus (Num 5) (BinOp Minus (Num 6) (Num 7)))) @?= "1 2 - 3 4 / * 5 6 7 - + +"
 
-unit_fromPostfix :: Assertion 
-unit_fromPostfix = do 
-    fromPostfix "123" @?= Just (Num 123) 
-    fromPostfix "1 2 * 3 +" @?= Just (BinOp Plus (BinOp Mult (Num 1) (Num 2)) (Num 3)) 
-    fromPostfix "1 2 - 3 4 / * 5 6 7 - + +" @?= Just (BinOp Plus (BinOp Mult (BinOp Minus (Num 1) (Num 2)) (BinOp Div (Num 3) (Num 4))) (BinOp Plus (Num 5) (BinOp Minus (Num 6) (Num 7)))) 
+unit_fromPostfix :: Assertion
+unit_fromPostfix = do
+    fromPostfix "123" @?= Just (Num 123)
+    fromPostfix "1 2 * 3 +" @?= Just (BinOp Plus (BinOp Mult (Num 1) (Num 2)) (Num 3))
+    fromPostfix "1 2 - 3 4 / * 5 6 7 - + +" @?= Just (BinOp Plus (BinOp Mult (BinOp Minus (Num 1) (Num 2)) (BinOp Div (Num 3) (Num 4))) (BinOp Plus (Num 5) (BinOp Minus (Num 6) (Num 7))))
     fromPostfix "13 42 +" @?= Just (BinOp Plus (Num 13) (Num 42))
     fromPostfix "1 2 3 +" @?= Nothing
-    fromPostfix "1 2 + *" @?= Nothing 
+    fromPostfix "1 2 + *" @?= Nothing
+    fromPostfix "1 2 %" @?= Nothing 
