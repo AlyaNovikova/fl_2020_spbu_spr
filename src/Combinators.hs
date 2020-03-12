bca8dd0359078444e5ddb5b6aa8315ffaa1720a1
AlyaNovikova <aleksnovikova@gmail.com>
2020-03-12 18:01:13 +0300

    hw03

--- src/Combinators.hs
+++ src/Combinators.hs
@@ -1,6 +1,7 @@
 module Combinators where
 
 import           Control.Applicative
+import           Control.Monad
 
 data Result error input result
   = Success input result
@@ -11,46 +12,38 @@ newtype Parser error input result
   = Parser { runParser :: input -> Result error input result }
 
 instance Functor (Parser error input) where
-  fmap = error "fmap not implemented"
+  fmap f p = Parser $ \inp -> case (runParser p inp) of
+      Success inp' res -> Success inp' (f res)
+      Failure err -> Failure err
+
 
 instance Applicative (Parser error input) where
-  pure = error "pure not implemented"
-  (<*>) = error "<*> not implemented"
+  pure = return
+  (<*>) = ap
 
 instance Monad (Parser error input) where
-  return = error "return not implemented"
+  return res = Parser $ \inp -> Success inp res
 
-  (>>=) = error ">>= not implemented"
+  p >>= f = Parser $ \inp -> case (runParser p inp) of
+      Success inp' res -> runParser (f res) inp'
+      Failure err -> Failure err
 
 instance Monoid error => Alternative (Parser error input) where
-  empty = error "empty not implemented"
+  empty = Parser $ \inp -> Failure mempty
 
-  (<|>) = error "<|> not implemented"
+  p1 <|> p2 = Parser $ \inp -> case (runParser p1 inp) of
+      Success inp' res -> Success inp' res
+      Failure _ -> runParser p2 inp
 
 -- Принимает последовательность элементов, разделенных разделителем
 -- Первый аргумент -- парсер для разделителя
 -- Второй аргумент -- парсер для элемента
 -- В последовательности должен быть хотя бы один элемент
-sepBy1 :: Parser e i sep -> Parser e i a -> Parser e i [a]
-sepBy1 sep elem = error "sepBy1 not implemented"
-
--- Альтернатива: в случае неудачи разбора первым парсером, парсит вторым
-alt' :: Parser e i a -> Parser e i a -> Parser e i a
-alt' p q = Parser $ \input ->
-  case runParser p input of
-    Failure _ -> runParser q input
-    x         -> x
-
--- Последовательное применение парсеров:
--- если первый парсер успешно принимает префикс строки, второй запускается на суффиксе.
--- Второй парсер использует результат первого.
-bind' :: Parser e i a
-      -> (a -> Parser e i b)
-      -> Parser e i b
-bind' p f = Parser $ \input ->
-  case runParser p input of
-    Success i r -> runParser (f r) i
-    Failure e   -> Failure e
+sepBy1 :: Monoid e => Parser e i sep -> Parser e i a -> Parser e i [a]
+sepBy1 sep elem = do
+    el <- elem
+    elems <- many $ sep *> elem 
+    return (el:elems)
 
 -- Проверяет, что первый элемент входной последовательности -- данный символ
 symbol :: Char -> Parser String String Char
@@ -79,22 +72,3 @@ success a = Parser $ \input -> Success input a
 -- Всегда завершается ошибкой
 fail' :: e -> Parser e i a
 fail' = Parser . const . Failure
-
--- Проверяет, что первый элемент входной последовательности -- данный символ
-fmap' :: (a -> b) -> Parser e i a -> Parser e i b
-fmap' f p = Parser $ \input ->
-  case runParser p input of
-    Success i a -> Success i (f a)
-    Failure e   -> Failure e
-
--- Последовательное применения одного и того же парсера 1 или более раз
-some' :: Parser e i a -> Parser e i [a]
-some' p =
-  p `bind'` \a ->
-  many' p `bind'` \as ->
-  success (a : as)
-
--- Последовательное применение одного и того же парсера 0 или более раз
-many' :: Parser e i a -> Parser e i [a]
-many' p =
-  some' p `alt'` success []
--- src/Expr.hs
+++ src/Expr.hs
@@ -1,9 +1,12 @@
 module Expr where
 
 import           AST         (AST (..), Operator (..))
-import           Combinators (Parser (..), Result (..), bind', elem', fail',
-                              fmap', satisfy, some', success)
+import           Combinators (Parser (..), Result (..), elem',
+                              fail', satisfy, success, symbol)
 import           Data.Char   (digitToInt, isDigit)
+
+import           Control.Applicative
+import           Control.Monad
 
 data Associativity
   = LeftAssoc  -- 1 @ 2 @ 3 @ 4 = (((1 @ 2) @ 3) @ 4)
@@ -16,24 +19,52 @@ uberExpr :: Monoid e
          -> Parser e i ast -- парсер для элементарного выражения
          -> (op -> ast -> ast -> ast) -- функция для создания абстрактного синтаксического дерева для бинарного оператора
          -> Parser e i ast
-uberExpr = error "uberExpr undefined"
+
+uberExpr [] termP _ = termP
+
+uberExpr opsP termP f = foldr helper termP opsP where
+    helper (op, LeftAssoc) term = do
+        t <- term
+        foldl (\v1 (o, v2) -> f o v1 v2) t <$> many ((,) <$> op <*> term)
+
+    helper (op, RightAssoc) term = right where
+        right = (do
+            t <- term
+            o <- op
+            t' <- right
+            return (f o t t')) <|> term
+
+    helper (op, NoAssoc) term = do
+        t <- term
+        ((\o -> f o t) <$> op <*> term) <|> return t
+
+
+plus  = symbol '+' >>= toOperator
+minus = symbol '-' >>= toOperator
+mult  = symbol '*' >>= toOperator
+divv  = symbol '/' >>= toOperator
+pow   = symbol '^' >>= toOperator
 
 -- Парсер для выражений над +, -, *, /, ^ (возведение в степень)
 -- с естественными приоритетами и ассоциативностью над натуральными числами с 0.
 -- В строке могут быть скобки
 parseExpr :: Parser String String AST
-parseExpr = error "parseExpr undefined"
+parseExpr = uberExpr [(plus <|> minus, LeftAssoc),
+                      (mult <|> divv, LeftAssoc),
+                      (pow, RightAssoc)]
+                     (Num <$> parseNum <|> symbol '(' *> parseExpr <* symbol ')')
+                     BinOp
 
 -- Парсер для натуральных чисел с 0
 parseNum :: Parser String String Int
-parseNum = foldl (\acc d -> 10 * acc + digitToInt d) 0 `fmap'` go
+parseNum = foldl (\acc d -> 10 * acc + digitToInt d) 0 `fmap` go
   where
     go :: Parser String String String
-    go = some' (satisfy isDigit)
+    go = some (satisfy isDigit)
 
 -- Парсер для операторов
 parseOp :: Parser String String Operator
-parseOp = elem' `bind'` toOperator
+parseOp = elem' >>= toOperator
 
 -- Преобразование символов операторов в операторы
 toOperator :: Char -> Parser String String Operator
@@ -55,4 +86,3 @@ compute (BinOp Plus x y)  = compute x + compute y
 compute (BinOp Mult x y)  = compute x * compute y
 compute (BinOp Minus x y) = compute x - compute y
 compute (BinOp Div x y)   = compute x `div` compute y
-
