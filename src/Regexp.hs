a6f2e5c31845379b90d26eb80a2425a9297195be
AlyaNovikova <aleksnovikova@gmail.com>
2020-05-13 23:26:12 +0300

    1 задание

--- src/Regexp.hs
+++ src/Regexp.hs
@@ -14,7 +14,24 @@ match :: Regexp -> String -> Bool
 match r s = nullable (foldl (flip derivative) r s)
 
 derivative :: Char -> Regexp -> Regexp
-derivative = error "derivative not implemented"
+derivative a reg = case reg of
+    Empty   -> Empty
+    Epsilon -> Empty
+    Char c  -> if (a == c) then Epsilon else Empty
+    Seq r s -> Alt (Seq (derivative a r) s) (toSet r (derivative a s))
+    Alt r s -> Alt (derivative a r) (derivative a s)
+    Star r  -> Seq (derivative a r) (Star r)
 
 nullable :: Regexp -> Bool
-nullable = error "nullable not implemented"
+nullable reg = case reg of
+    Empty   -> False
+    Epsilon -> True
+    Char _  -> False
+    Seq r s -> (nullable r) && (nullable s)
+    Alt r s -> (nullable r) || (nullable s)
+    Star _  -> True
+
+-- конкатенируем вручную :))))
+toSet :: Regexp -> Regexp -> Regexp
+toSet r s | (nullable r) = s
+          | otherwise = Empty
