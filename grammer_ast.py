c58877b977ca25abbed12e2daa84fb8b69828683
AlyaNovikova <aleksnovikova@gmail.com>
2020-05-17 12:58:06 +0300

    python 3.6

--- grammer_ast.py
+++ grammer_ast.py
@@ -1,11 +1,10 @@
-from typing import List
-from dataclasses import dataclass
-
-
-@dataclass
 class Rule:
-    nonterm: str
-    seq: List[str]
+    def __init__(self, nonterm, seq):
+        self.nonterm = nonterm
+        self.seq = seq
+
+    def __eq__(self, other):
+        return self.nonterm == other.nonterm and self.seq == other.seq
 
     def __hash__(self):
         return hash((self.nonterm, tuple(self.seq)))
